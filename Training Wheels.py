c3787a77af9919b70936ccd755a599f949797ee9
Turan Ates <jamesdusky@dustytavern.com>
2020-09-04 12:13:09 -0400

    [A[A[B[Btest

--- Training Wheels.py
+++ Training Wheels.py
@@ -3,6 +3,11 @@ from discord.ext import commands
 client = discord.Client()
 bot = commands.Bot(command_prefix='$')
 
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
 @bot.command()
 async def foo(ctx, arg):
     await ctx.send(arg)
