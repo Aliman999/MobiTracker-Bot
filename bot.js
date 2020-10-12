0a3a6dc5176176f6ba3395c477ea5c1ad62e9d1c
Turan Ates <jamesdusky@dustytavern.com>
2020-10-11 21:25:30 -0400

    t

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.on('message', message => {
   const args = message.content.slice(prefix.length).trim().split(' ');
   const command = args.shift().toLowerCase();
 
-  else if (command === 'args-info'){
+  if (command === 'args-info'){
   	if (!args.length){
   		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
   	}
