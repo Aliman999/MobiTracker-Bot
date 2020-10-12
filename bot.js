a75164a78c0b6fcf093967f40fb88c7d7936f2ff
Turan Ates <jamesdusky@dustytavern.com>
2020-10-12 18:49:08 -0400

    t

--- bot.js
+++ bot.js
@@ -58,8 +58,10 @@ client.on('message', message => {
       path: '/c13b1badf9ccd433c90b4160c7664107/v1/auto/user/'+`${args}`,
       method: 'GET'
     }
-    message.channel.type = (`"dm"`);
-    message.author.send(`"Your other message"`);
+    //THIS IS FOR ALERTS
+    //message.channel.type = (`"dm"`);
+    //message.author.send(`"Your other message"`);
+    //THIS IS FOR ALERTS
     const req = https.request(options, res => {
       console.log('Looked up '+`${args}`);
       res.on('data', d => {
