e6a75b56544d02307f3d8542920841f82e7adca8
Turan Ates <jamesdusky@dustytavern.com>
2020-10-11 21:33:36 -0400

    t

--- bot.js
+++ bot.js
@@ -20,15 +20,8 @@ client.on('message', message => {
   	}else if (args.length > 1) {
   		return message.channel.send(`Too many arguments!, ${message.author}!`);
     }
-  	message.channel.send(`Command name: ${command}\nArguments: ${args}`);
-  }
+  	//message.channel.send(`Command name: ${command}\nArguments: ${args}`);
 
-  if (message.content === `${prefix}server`) {
-	   message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-  }
-  if (!message.content.startsWith(`${prefix}`)) return;
-  // If the message is "how to embed"
-  if (message.content === 'how to embed') {
     // We can create embeds using the MessageEmbed constructor
     // Read more about all that you can do with the constructor
     // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
@@ -38,14 +31,20 @@ client.on('message', message => {
       //This query is from mobitracker.co
       .setAuthor('Mobitracker.co', 'https://mobitracker.co/android-chrome-192x192.png')
       // Set the title of the field
-      .setTitle('JamesDusky - MobiTracker.co')
+      .setTitle(`${args}`+' - MobiTracker.co')
       // URL to the client's Profile
-      .setURL("https://mobitracker.co/JamesDusky")
+      .setURL("https://mobitracker.co/"+`${args}`)
       // Set the main content of the embed
       .setDescription('');
     // Send the embed to the same channel as the message
     message.channel.send(embed);
   }
+
+  if (message.content === `${prefix}server`) {
+	   message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+  }
+  if (!message.content.startsWith(`${prefix}`)) return;
+  // If the message is "how to embed"
 });
 
 client.login(config.Key);
