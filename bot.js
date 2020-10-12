b1a09730d10616d1ddc6826b69c626dd9f40bd6a
Turan Ates <jamesdusky@dustytavern.com>
2020-10-11 21:22:04 -0400

    t

--- bot.js
+++ bot.js
@@ -12,27 +12,28 @@ client.on('message', message => {
   if (message.content === 'ping') {
     message.channel.send('pong');
   }
-
-  if (message.content.startsWith(prefix)) {
-    // If the message is "how to embed"
-    if (message.content === 'how to embed') {
-      // We can create embeds using the MessageEmbed constructor
-      // Read more about all that you can do with the constructor
-      // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
-      const embed = new MessageEmbed()
-        // Set the color of the embed
-        .setColor(0x39ced8)
-        //This query is from mobitracker.co
-        .setAuthor('Mobitracker.co', 'https://mobitracker.co/android-chrome-192x192.png')
-        // Set the title of the field
-        .setTitle('JamesDusky - MobiTracker.co')
-        // URL to the client's Profile
-        .setURL("https://mobitracker.co/JamesDusky")
-        // Set the main content of the embed
-        .setDescription('');
-      // Send the embed to the same channel as the message
-      message.channel.send(embed);
-    }
+  if (message.content === `${prefix}server`) {
+	   message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+  }
+  if (!message.content.startsWith(${prefix})) return;
+  // If the message is "how to embed"
+  if (message.content === 'how to embed') {
+    // We can create embeds using the MessageEmbed constructor
+    // Read more about all that you can do with the constructor
+    // over at https://discord.js.org/#/docs/main/master/class/MessageEmbed
+    const embed = new MessageEmbed()
+      // Set the color of the embed
+      .setColor(0x39ced8)
+      //This query is from mobitracker.co
+      .setAuthor('Mobitracker.co', 'https://mobitracker.co/android-chrome-192x192.png')
+      // Set the title of the field
+      .setTitle('JamesDusky - MobiTracker.co')
+      // URL to the client's Profile
+      .setURL("https://mobitracker.co/JamesDusky")
+      // Set the main content of the embed
+      .setDescription('');
+    // Send the embed to the same channel as the message
+    message.channel.send(embed);
   }
 });
 
