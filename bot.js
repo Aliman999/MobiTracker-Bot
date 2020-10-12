e3761b967464e0d2966ae86670f5f27c1ae7d436
Turan Ates <jamesdusky@dustytavern.com>
2020-10-12 17:05:52 -0400

    t

--- bot.js
+++ bot.js
@@ -62,7 +62,7 @@ client.on('message', message => {
       console.log('Looked up '+`${args}`);
       res.on('data', d => {
         const user = JSON.parse(d);
-        if(Object.size(user.data.oranization.name) > 0){
+        if(Object.size(user.data.oranization) > 0){
           user.data.organization.name = user.data.organization.rank+' in '+'['+user.data.organization.name;
         }else{
           user.data.organization.name = "REDACTED";
