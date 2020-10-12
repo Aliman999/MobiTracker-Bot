330ad8d1ed10a4d0197383473d534ad321b83657
Turan Ates <jamesdusky@dustytavern.com>
2020-10-12 16:18:27 -0400

    t

--- bot.js
+++ bot.js
@@ -57,9 +57,9 @@ client.on('message', message => {
         if(!user.data.profile.title){
           user.data.profile.title = "None";
         }
-        user.data.profile.id = user.data.profile.id.substring(1);
+        const cID = user.data.profile.id.substring(1);
         console.log(user.data.profile.id);
-        const sql = "SELECT avgRating as rating, reviewed_count as count FROM players WHERE username = '"+user.data.profile.handle+"'"+" AND cID = "+user.data.profile.id;
+        const sql = "SELECT avgRating as rating, reviewed_count as count FROM players WHERE username = '"+user.data.profile.handle+"'"+" AND cID = "+cID;
         con.query(sql, function (err, result, fields) {
           if (err) throw err;
           const embed = new MessageEmbed()
