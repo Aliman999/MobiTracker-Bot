64143f05ee08523e2d3d4b6344a9a10689e42713
Turan Ates <jamesdusky@dustytavern.com>
2020-10-14 00:00:13 -0400

    t

--- bot.js
+++ bot.js
@@ -4,7 +4,29 @@ const config  = require('./config');
 const prefix = '!';
 const client = new Client();
 const https = require('https');
-var mysql = require('mysql');
+const mysql = require('mysql');
+const WebSocket = require('ws');
+
+function heartbeat() {
+  clearTimeout(this.pingTimeout);
+
+  // Use `WebSocket#terminate()`, which immediately destroys the connection,
+  // instead of `WebSocket#close()`, which waits for the close timer.
+  // Delay should be equal to the interval at which your server
+  // sends out pings plus a conservative assumption of the latency.
+  this.pingTimeout = setTimeout(() => {
+    this.terminate();
+  }, 30000 + 1000);
+}
+
+const client = new WebSocket('wss://mobitracker.co/:8000');
+
+client.on('open', heartbeat);
+client.on('ping', heartbeat);
+client.on('close', function clear() {
+  clearTimeout(this.pingTimeout);
+});
+
 
 var con = mysql.createConnection({
   host: config.MysqlHost,
