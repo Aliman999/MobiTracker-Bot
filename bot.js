21bcb7884227a350da07290cbbbeaeae7a3c5422
Turan Ates <jamesdusky@dustytavern.com>
2020-10-15 04:07:57 -0400

    t

--- bot.js
+++ bot.js
@@ -3,10 +3,10 @@ const Discord = require('discord.js');
 const config  = require('./config');
 const prefix = '!';
 const fs = require('fs');
-const client = new Discord.Client();
 const https = require('https');
 const mysql = require('mysql');
 const WebSocket = require('ws');
+var client = new Discord.Client();
 var wsClient;
 var jwt = require('jsonwebtoken');
 var discordClients = [];
@@ -100,128 +100,127 @@ Object.size = function(obj) {
   return size;
 };
 
-function ready(){
-  client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    console.log(message);
-
-    if (command === 'search'){
-    	if (!args.length){
-    		return message.channel.send(`You didnt provide a username.`);
-    	}else if (args.length > 1) return message.channel.send(`Too many arguments.`);
-      const options = {
-        hostname: 'api.starcitizen-api.com',
-        port: 443,
-        path: '/c13b1badf9ccd433c90b4160c7664107/v1/auto/user/'+`${args}`,
-        method: 'GET'
-      }
-      const req = https.request(options, res => {
-        console.log('Looked up '+`${args}`);
-        res.on('data', d => {
-          const user = JSON.parse(d);
-          if(Object.size(user.data) > 0){
-            if(Object.size(user.data.organization) > 1){
-              user.data.organization.name = user.data.organization.rank+' in '+'['+user.data.organization.name+'](https://robertsspaceindustries.com/orgs/'+user.data.organization.sid+')';
-            }else if (user.data.organization.name == "") {
-              user.data.organization.name = "REDACTED";
-            }else {
-              user.data.organization.name = "None";
-            }
-            const cID = user.data.profile.id.substring(1);
-            const sql = "SELECT avgRating as rating, reviewed_count as count FROM players WHERE username = '"+user.data.profile.handle+"'"+" AND cID = "+cID;
-            con.query(sql, function (err, result, fields) {
-              if (err) throw err;
-              var rating = "";
-              if(result.length == 0){
-                rating = "No Reviews. \n[Login](https://mobitracker.co/login) to leave them a review.";
-              }else{
-                if(result[0].rating == -1){
-                  rating = "No Reviews. \n[Login](https://mobitracker.co/login) to leave them a review.";
-                }else{
-                  rating = result[0].rating+"/5 "+"("+result[0].count+")";
-                }
-              }
-              const embed = new MessageEmbed()
-                .setColor(0x25a6dd)
-                .setAuthor(user.data.profile.handle+" "+user.data.profile.id, user.data.profile.image, "https://mobitracker.co/"+user.data.profile.handle)
-                .setDescription("AKA "+user.data.profile.display)
-                .setThumbnail(user.data.profile.image)
-                .addFields(
-                  { name: 'Badge', value: user.data.profile.badge, inline: true},
-                  { name: 'Mobitracker Rating', value: rating, inline: true},
-                  { name: 'Main Organization', value: user.data.organization.name },
-                  { name: 'Affiliated Organizations', value: affiliations(user.data.affiliation)}
-                 )
-                 .setFooter(user.data.profile.handle+' - Mobitracker.co', 'https://mobitracker.co/android-chrome-512x512.png');
-              message.channel.send(embed);
-            });
-          }else{
-            message.channel.send(`That user doesnt exist.`);
-          }
-          ready();
-        })
-      })
-
-      req.on('error', error => {
-        console.error(error)
-      })
-
-      req.end()
-
-    }else if(command == 'auth'){
-      if(!args.length){
-        return message.channel.send('Sign in at https://mobitracker.co/login and click the button that says "Authenticate with Discord". \nThen and copy the text provided and paste it here.');
-      }else if(args.length>1){
-        return message.channel.send('Too many arguments.');
-      }else{
-        jwt.verify(`${args}`, config.Secret, { algorithm: 'HS265' }, function (err, decoded){
-          if(err){
-            console.log(err);
-          }else{
-            if(decoded.cid != "" && decoded.username != ""){
-              const authUser = message.channel;
-              delete authUser.lastMessageChannelID;
-              const token = jwt.sign({ mtUser: { cid:decoded.cid, username:decoded.username }, discordUser: authUser}, config.Secret, { algorithm: 'HS256' }, { 'iat':Math.floor(Date.now()/1000) });
-              const msg = {
-                type:"authDiscord",
-                token: token
-              };
-              wsClient.send(JSON.stringify(msg));
-              wsClient.on('message', function(response){
-                response = JSON.parse(response);
-                if(response.data == 'success'){
-                  message.channel.send('Your discord is now linked with '+decoded.username+' \nhttps://mobitracker.co/'+decoded.username+' \nRemmember to share a server containing this bot to keep getting alerts! \nYou may toggle alerts with !alerts.');
-                }else if(response.data == 'exists'){
-                  message.channel.send('Your account is already linked.');
-                }else if(response.data == 'nonexists'){
-                  message.channel.send('You must sign up at https://mobitracker.co/register To get discord alerts.');
-                }
-                response = "";
-              });
-            }else{
-              message.channel.send('The token was invalid. Please copy the provided token from https://mobitracker.co/auth');
-            }
-            ready();
-          }
-        });
-      }
-    }else if(command == 'alerts'){
-
-    }
-    //message.channel.send("This is MobiTracker.co 's official Discord bot. \nCurrent Commands: \n!search RSI_HANDLE \n !auth TOKEN - This token is received from https://mobitracker.co/auth \n!alerts'");
-    if (!message.content.startsWith(`${prefix}`)) return;
-  });
-}
-
 client.on('ready', () => {
   console.log('MobiTracker Bot is Ready');
-  ready();
 });
 
+async function reset(){
+  client = new Discord.Client();
+}
 
+client.on('message', message => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  console.log(message);
+
+  if (command === 'search'){
+  	if (!args.length){
+  		return message.channel.send(`You didnt provide a username.`);
+  	}else if (args.length > 1) return message.channel.send(`Too many arguments.`);
+    const options = {
+      hostname: 'api.starcitizen-api.com',
+      port: 443,
+      path: '/c13b1badf9ccd433c90b4160c7664107/v1/auto/user/'+`${args}`,
+      method: 'GET'
+    }
+    const req = https.request(options, res => {
+      console.log('Looked up '+`${args}`);
+      res.on('data', d => {
+        const user = JSON.parse(d);
+        if(Object.size(user.data) > 0){
+          if(Object.size(user.data.organization) > 1){
+            user.data.organization.name = user.data.organization.rank+' in '+'['+user.data.organization.name+'](https://robertsspaceindustries.com/orgs/'+user.data.organization.sid+')';
+          }else if (user.data.organization.name == "") {
+            user.data.organization.name = "REDACTED";
+          }else {
+            user.data.organization.name = "None";
+          }
+          const cID = user.data.profile.id.substring(1);
+          const sql = "SELECT avgRating as rating, reviewed_count as count FROM players WHERE username = '"+user.data.profile.handle+"'"+" AND cID = "+cID;
+          con.query(sql, function (err, result, fields) {
+            if (err) throw err;
+            var rating = "";
+            if(result.length == 0){
+              rating = "No Reviews. \n[Login](https://mobitracker.co/login) to leave them a review.";
+            }else{
+              if(result[0].rating == -1){
+                rating = "No Reviews. \n[Login](https://mobitracker.co/login) to leave them a review.";
+              }else{
+                rating = result[0].rating+"/5 "+"("+result[0].count+")";
+              }
+            }
+            const embed = new MessageEmbed()
+              .setColor(0x25a6dd)
+              .setAuthor(user.data.profile.handle+" "+user.data.profile.id, user.data.profile.image, "https://mobitracker.co/"+user.data.profile.handle)
+              .setDescription("AKA "+user.data.profile.display)
+              .setThumbnail(user.data.profile.image)
+              .addFields(
+                { name: 'Badge', value: user.data.profile.badge, inline: true},
+                { name: 'Mobitracker Rating', value: rating, inline: true},
+                { name: 'Main Organization', value: user.data.organization.name },
+                { name: 'Affiliated Organizations', value: affiliations(user.data.affiliation)}
+               )
+               .setFooter(user.data.profile.handle+' - Mobitracker.co', 'https://mobitracker.co/android-chrome-512x512.png');
+            message.channel.send(embed);
+          });
+        }else{
+          message.channel.send(`That user doesnt exist.`);
+        }
+        reset();
+      })
+    })
+
+    req.on('error', error => {
+      console.error(error)
+    })
+
+    req.end()
+
+  }else if(command == 'auth'){
+    if(!args.length){
+      return message.channel.send('Sign in at https://mobitracker.co/login and click the button that says "Authenticate with Discord". \nThen and copy the text provided and paste it here.');
+    }else if(args.length>1){
+      return message.channel.send('Too many arguments.');
+    }else{
+      jwt.verify(`${args}`, config.Secret, { algorithm: 'HS265' }, function (err, decoded){
+        if(err){
+          console.log(err);
+        }else{
+          if(decoded.cid != "" && decoded.username != ""){
+            const authUser = message.channel;
+            delete authUser.lastMessageChannelID;
+            const token = jwt.sign({ mtUser: { cid:decoded.cid, username:decoded.username }, discordUser: authUser}, config.Secret, { algorithm: 'HS256' }, { 'iat':Math.floor(Date.now()/1000) });
+            const msg = {
+              type:"authDiscord",
+              token: token
+            };
+            wsClient.send(JSON.stringify(msg));
+            wsClient.on('message', function(response){
+              response = JSON.parse(response);
+              if(response.data == 'success'){
+                message.channel.send('Your discord is now linked with '+decoded.username+' \nhttps://mobitracker.co/'+decoded.username+' \nRemmember to share a server containing this bot to keep getting alerts! \nYou may toggle alerts with !alerts.');
+              }else if(response.data == 'exists'){
+                message.channel.send('Your account is already linked.');
+              }else if(response.data == 'nonexists'){
+                message.channel.send('You must sign up at https://mobitracker.co/register To get discord alerts.');
+              }
+              response = "";
+            });
+          }else{
+            message.channel.send('The token was invalid. Please copy the provided token from https://mobitracker.co/auth');
+          }
+        }
+        reset();
+      });
+    }
+  }else if(command == 'alerts'){
+
+  }
+  //message.channel.send("This is MobiTracker.co 's official Discord bot. \nCurrent Commands: \n!search RSI_HANDLE \n !auth TOKEN - This token is received from https://mobitracker.co/auth \n!alerts'");
+  if (!message.content.startsWith(`${prefix}`)) return;
+});
 
 client.login(config.Key);
