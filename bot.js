a7fcdca8bbed996b4e2f1f73971215d9b710ffdb
Turan Ates <jamesdusky@dustytavern.com>
2020-10-15 03:23:38 -0400

    t

--- bot.js
+++ bot.js
@@ -104,7 +104,7 @@ client.on('ready', () => {
 });
 
 async function reply(message, args){
-    message.author.send(args).catch(e => {
+    message.member.user.send(args).catch(e => {
         message.channel.send("It seems you dont have DM's enabled. We wont beable to contact you with DM's disabled. \n"+`${message.author}`);
     });
 }
