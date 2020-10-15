e92802bf04d3378d096d4aed3091d1dbaae60a5e
Turan Ates <jamesdusky@dustytavern.com>
2020-10-15 03:35:43 -0400

    t

--- bot.js
+++ bot.js
@@ -227,7 +227,7 @@ client.on('message', message => {
   }
   //message.channel.send("This is MobiTracker.co 's official Discord bot. \nCurrent Commands: \n!search RSI_HANDLE \n !auth TOKEN - This token is received from https://mobitracker.co/auth \n!alerts'");
   if (!message.content.startsWith(`${prefix}`)) return;
-  delete message;
+  message = null;
 });
 
 client.login(config.Key);
