d2632dd2b7642d4d2aa6d1c684c1d4657a803eb7
Turan Ates <jamesdusky@dustytavern.com>
2020-10-19 19:55:08 -0400

    t

--- bot.js
+++ bot.js
@@ -168,7 +168,7 @@ client.on('message', message => {
           }
           var cID = '';
           if(user.data.profile.id != 'n/a'){
-            const cID = 'AND cID ='+user.data.profile.id.substring(1);
+            cID = 'AND cID ='+user.data.profile.id.substring(1);
           }
           const sql = "SELECT avgRating as rating, reviewed_count as count FROM players WHERE username = '"+user.data.profile.handle+"'"+cID;
           con.query(sql, function (err, result, fields) {
