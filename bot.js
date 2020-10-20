c40114ef757a1394e49f6d0edf0d05f6449ecc17
Turan Ates <jamesdusky@dustytavern.com>
2020-10-20 10:39:00 -0400

    t

--- bot.js
+++ bot.js
@@ -277,7 +277,7 @@ client.on('message', message => {
     var mp;
     var limit;
     if(p*pp == 0){
-      limit = 'LIMIT 4';
+      limit = 'LIMIT 4, '+p+1;
     }else{
       limit = 'LIMIT 4, '+p*pp;
     }
