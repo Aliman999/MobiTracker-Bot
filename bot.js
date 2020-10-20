a23e1d6b616b4a25949e12ef7ece57a8c716779f
Turan Ates <jamesdusky@dustytavern.com>
2020-10-20 10:53:04 -0400

    t

--- bot.js
+++ bot.js
@@ -95,7 +95,7 @@ function affiliations(aff){
 
 String.decodeHTML = function() {
     var map = {"gt":">" /* , … */};
-    return this.replace(/&(#(?:x[0-9a-f]+|\d+)|[a-z]+);?/gi, function($0, $1) {
+    return this.toString().replace(/&(#(?:x[0-9a-f]+|\d+)|[a-z]+);?/gi, function($0, $1) {
         if ($1[0] === "#") {
             return String.fromCharCode($1[1].toLowerCase() === "x" ? parseInt($1.substr(2), 16)  : parseInt($1.substr(1), 10));
         } else {
@@ -353,7 +353,7 @@ client.on('message', message => {
           }else{
             result[x].duration = result[x].duration+' hours';
           }
-          String.decodeHTML(result[x].unsecure.toString());
+          String.decodeHTML(result[x].unsecure);
           result[x].unsecure = truncate(result[x].unsecure, 10);
         }
         p++;
