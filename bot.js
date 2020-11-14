8d016a596efba1838c94a6213b1f0959f38bd015
Turan Ates <jamesdusky@dustytavern.com>
2020-11-14 13:11:41 -0500

    t

--- bot.js
+++ bot.js
@@ -216,7 +216,7 @@ client.on('message', message => {
     }
     jwt.verify(`${args}`, config.Secret, { algorithm: 'HS265' }, function (err, decoded){
       if(err){
-        console.log(this);
+        console.log(err);
       }else{
         if(decoded.cid != "" && decoded.username != ""){
           const authUser = message.author;
