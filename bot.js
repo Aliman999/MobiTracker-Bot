87494fe13194e095c1be23c8e4034ca3b1eb7d16
Turan Ates <jamesdusky@dustytavern.com>
2020-11-14 13:03:30 -0500

    t

--- bot.js
+++ bot.js
@@ -236,7 +236,7 @@ client.on('message', message => {
               con.query(sql, function (err, result, fields) {
                 if (err) throw err;
                 if(result.length > 0){
-                  console.log(result[0]);
+                  console.log(decoded);
                   if(decoded.contracts == result[0].contracts && decoded.applicants == result[0].applicants && decoded.reviews == result[0].reviews ){
                     message.author.send('Your policies are the same.');
                   }else{
