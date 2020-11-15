199783422cd43a725d724505336cabc5e121d02f
Turan Ates <jamesdusky@dustytavern.com>
2020-11-15 18:07:01 -0500

    t

--- bot.js
+++ bot.js
@@ -485,7 +485,7 @@ const program = async () => {
 
         var index = Object.keys(show);
         for(var i = 0; i < Object.keys(show).length; i++){
-          if(show[index[i]].active){
+          if(show[index[i]].active && show[index[i]].active){
             var title = index[i].charAt(0).toUpperCase() + index[i].slice(1);
             embed.addFields({ name: show[index[i]].count+" New "+title, value: "Test", inline: false });
             embed.addFields({ name: '\u200B', value: '\u200B' });
