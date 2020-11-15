bc35533933d686c3ed93202e95a2fdeae3d0d346
Turan Ates <jamesdusky@dustytavern.com>
2020-11-15 16:25:26 -0500

    t

--- bot.js
+++ bot.js
@@ -473,7 +473,7 @@ const program = async () => {
           .setAuthor('MobiTracker Notifications', 'https://mobitracker.co/android-chrome-512x512.png', 'https://mobitracker.co/'+alertAfter.username)
           .setFooter(alertAfter.username+' - Mobitracker.co');
 
-        console.log(Object.keys(show));
+        console.log(show);
         for (var i = 0; i < show.length; i++) {
           if(show[Object.keys(show)[i]].active){
             embed.addFields({ name: "Test", value: "This Works", inline: true });
