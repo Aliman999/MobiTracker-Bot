fe5fb318c3902caa5f4299a46417a83e9f30c010
Turan Ates <jamesdusky@dustytavern.com>
2020-11-15 15:44:29 -0500

    t

--- bot.js
+++ bot.js
@@ -466,7 +466,7 @@ const program = async () => {
         const alertAfter = event.affectedRows[0].after;
         const user = event.affectedRows[0].after.discordUser;
         const id = JSON.parse(user);
-        console.log(client.users.cache);
+        console.log(client.users);
 
         client.users.cache.get(id.id).send("test");
         /*
