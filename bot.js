9bb17928b482145958654fe969be49f150acf117
Turan Ates <jamesdusky@dustytavern.com>
2020-11-17 17:49:34 -0500

    t

--- bot.js
+++ bot.js
@@ -490,7 +490,8 @@ const program = async () => {
         for(var i = 0; i < Object.keys(show).length; i++){
           if(show[index[i]].active){
             if(i == 1){
-              var title = [ index[i].charAt(0).toUpperCase() + index[i].slice(1), index[i].charAt(0).toUpperCase() + index[i].slice(1) ];
+              var title = [ index[i].myApplications.charAt(0).toUpperCase() + index[i].slice(1), index[i].myContracts.charAt(0).toUpperCase() + index[i].slice(1) ];
+              console.log(title);
             }else{
               var title = index[i].charAt(0).toUpperCase() + index[i].slice(1);
             }
