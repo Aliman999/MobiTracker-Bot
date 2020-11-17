91391cec2df7b0af7c6fe58708e9c5686915d1e3
Turan Ates <jamesdusky@dustytavern.com>
2020-11-17 18:54:55 -0500

    t

--- bot.js
+++ bot.js
@@ -494,12 +494,12 @@ const program = async () => {
               if(i == 1){
                 console.log(show[index[i]].myApplications.events[(show[index[i]].myApplications.events.length-1)]);
                 embed.addFields({ name: title+" - "+show[index[i]].count, value:"\u200B" });
-                embed.addFields({ name: "My Applications - "+show[index[i]].myApplications.events.length, value: "\u200B" });
-                embed.addFields({ name: "Latest", value: +show[index[i]].myApplications.events[(show[index[i]].myApplications.events.length-1)], inline: true });
+                embed.addFields({ name: "My Applications - "+show[index[i]].myApplications.events.length, value: "\u200B", inline: true });
+                embed.addFields({ name: "Latest", value: show[index[i]].myApplications.events[(show[index[i]].myApplications.events.length-1)], inline: true });
                 if(show[index[i]].myApplications.events.length>1){
                   embed.addFields({ name: "Previous", value: show[index[i]].myApplications.events[(show[index[i]].myApplications.events.length-2)], inline: true });
                 }
-                embed.addFields({ name: "My Contracts - "+show[index[i]].myContracts.events.length, value: "\u200B" });
+                embed.addFields({ name: "My Contracts - "+show[index[i]].myContracts.events.length, value: "\u200B", inline: true });
                 embed.addFields({ name: "Latest", value: show[index[i]].myContracts.events[(show[index[i]].myContracts.events.length-1)], inline: true });
                 if(show[index[i]].myContracts.events.length>1){
                   embed.addFields({ name: "Previous", value: show[index[i]].myContracts.events[(show[index[i]].myContracts.events.length-2)], inline: true });
