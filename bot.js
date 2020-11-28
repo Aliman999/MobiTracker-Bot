0d9cecdc82c44d13e575208874da879470a8ebe2
Turan Ates <jamesdusky@dustytavern.com>
2020-11-28 12:57:10 -0500

    t

--- bot.js
+++ bot.js
@@ -191,7 +191,7 @@ client.on('message', message => {
               .addFields(
                 { name: 'Badge', value: user.data.profile.badge, inline: true},
                 { name: 'Mobitracker Rating', value: rating, inline: true},
-                { name: 'RSI Profile', value: "[Here](https://robertsspaceindustries.com/citizens/"+user.data.profile.handle+")" }
+                { name: 'RSI Profile', value: "[Here](https://robertsspaceindustries.com/citizens/"+user.data.profile.handle+")", inline: true },
                 { name: 'Main Organization', value: user.data.organization.name },
                 { name: 'Affiliated Organizations', value: affiliations(user.data.affiliation)}
                )
