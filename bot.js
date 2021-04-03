411a09f1b0740c2437d8609701b85d9064ab999c
Turan Ates <jamesdusky@dustytavern.com>
2021-04-03 17:37:01 -0400

    t

--- bot.js
+++ bot.js
@@ -157,7 +157,7 @@ function queryApi(message, args){
               { name: 'Affiliated Organizations', value: affiliations(user.data.affiliation)}
              )
              .setFooter(user.data.profile.handle+' - Mobitracker.co', 'https://mobitracker.co/android-chrome-512x512.png');
-             console.log(embed);
+          message.channel.send(embed);
           result = embed;
         });
       }else{
