d9f54dba63afa70053fa88be9df9ed8c03dc2686
Turan Ates <jamesdusky@dustytavern.com>
2021-04-03 14:36:57 -0400

    t

--- bot.js
+++ bot.js
@@ -211,7 +211,7 @@ client.on('message', message => {
   }
   if (!message.content.startsWith(prefix)) return;
 
-  const args = message.content.slice(prefix.length).trim(); //.split(/ +/)
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
 
   if (command === 'search'){
