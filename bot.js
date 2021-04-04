0867fe0b7ff09728d5e1aba780cc04336067af94
Turan Ates <jamesdusky@dustytavern.com>
2021-04-03 22:50:25 -0400

    t

--- bot.js
+++ bot.js
@@ -245,7 +245,7 @@ client.on('message', message => {
       }
     }else{
       if(message.member.user.tag != "MobiTracker#2117"){
-        console.log(new Date().toLocaleString()+" - "+message.member.user.tag+' Looked up '+args[i]+' in the '+message.guild.name+' server');
+        console.log(new Date().toLocaleString()+" - "+message.member.user.tag+' Looked up '+args+' in the '+message.guild.name+' server');
       }
       lookUp(message, args);
     }
