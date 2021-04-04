ec74fc9f8dba7de92b744e2db7aeb067afbf2475
Turan Ates <jamesdusky@dustytavern.com>
2021-04-03 20:59:11 -0400

    t

--- bot.js
+++ bot.js
@@ -104,7 +104,8 @@ async function lookUp(message, args, finished = false){
 function queryApi(message, args){
   return new Promise(promiseSearch =>{
     var embed;
-    args.replace("—", "_");
+    console.log(args);
+    //args.replace("—", "_");
     const options = {
       hostname: 'api.starcitizen-api.com',
       port: 443,
