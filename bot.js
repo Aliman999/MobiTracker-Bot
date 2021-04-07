55914ebb148ce6d3e6435750aabf1a1ef4bfd054
Turan Ates <jamesdusky@dustytavern.com>
2021-04-07 17:55:13 -0400

    t

--- bot.js
+++ bot.js
@@ -26,7 +26,7 @@ const msg = {
 };
 
 function selectKey(){
-  console.log(Math.random() * (apiKeys.length - 0) + 0);
+  console.log(apiKeys[Math.floor(Math.random() * apiKeys.length)]);
 }
 selectKey();
 function socket(){
