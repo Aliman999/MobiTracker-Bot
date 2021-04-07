86d3c112c00646875d9c285ca54bfefc7f173cba
Turan Ates <jamesdusky@dustytavern.com>
2021-04-07 15:57:32 -0400

    t

--- bot.js
+++ bot.js
@@ -349,6 +349,7 @@ function cachePlayer(user){
     check.badge = JSON.stringify(check.badge);
     check.organization = JSON.stringify(Object.assign({}, check.organization));
     var eventString = eventUpdate.join();
+    console.log(eventString);
     const sql = "INSERT INTO `CACHE players` (event, cID, username, badge, organization, avatar) VALUES ('"+eventString+"', "+check.cID+", '"+check.username+"', '"+check.badge+"', '"+check.organization+"', '"+check.avatar+"' );";
     console.log(sql);
     //con.query(sql, function (err, result, fields) {
