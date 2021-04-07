4ad516f21d517a3805c8bdfbebb85bfe482b7e8f
Turan Ates <jamesdusky@dustytavern.com>
2021-04-07 14:04:04 -0400

    t

--- bot.js
+++ bot.js
@@ -303,7 +303,8 @@ function cachePlayer(user){
   //console.log(check);
   const sql = "SELECT cID, username, badge, organization, avatar, bio FROM `CACHE players` WHERE cID = "+user.profile.id.substring(1)+";";
   con.query(sql, function (err, result, fields) {
-    console.log(result[result.length-1]);
+    var data = result[result.length-1];
+    console.log(data);
     /*
     result[result.lenght-1].organization = JSON.parse(result[result.lenght-1].organization);
     result[result.lenght-1].badge = JSON.parse(result[result.lenght-1].badge);
