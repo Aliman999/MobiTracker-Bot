301074bab7cc1ce478fcfe5225a150ca01459c01
Turan Ates <jamesdusky@dustytavern.com>
2021-04-07 14:42:14 -0400

    t

--- bot.js
+++ bot.js
@@ -311,10 +311,9 @@ function cachePlayer(user){
     console.log(check);
     console.log(Object.keys(data));
     for(var i = 0; i < Object.size(data); i++){
-      console.log(i);
       if(i == 3){
-        for(var ii = 0; ii < Object.size(data.organization); ii++){
-          console.log(data[Object.keys(data)[ii]]);
+        for(var x = 0; x < Object.size(data.organization); x++){
+          console.log(x);
         }
       }else{
 
