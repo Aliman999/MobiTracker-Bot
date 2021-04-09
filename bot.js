0c68a80e56f643b5eaa0315f10aaec9dd07fd38d
Turan Ates <jamesdusky@dustytavern.com>
2021-04-09 09:52:17 -0400

    t

--- bot.js
+++ bot.js
@@ -361,7 +361,7 @@ async function registerUser(message, args){
                   }
                 }else{
                   retry();
-                  console.log("Failed to query "+args[i]+", retrying.");
+                  console.log("Failed to query "+args+", retrying.");
                 }
               })
             })
