c473e5f7a78a5b1eeefffbe8b2bcad301df9e8f2
Turan Ates <jamesdusky@dustytavern.com>
2021-04-08 23:33:48 -0400

    t

--- bot.js
+++ bot.js
@@ -339,11 +339,7 @@ async function registerUser(message, args){
       req.on('error', error => {
         console.error(error)
       });
-
-      req.on('end', () => {
-        console.log(registeredNames);
-        console.log(failedNames);
-      });
+      req.end();
     }
   }else{
     const registerP1 = "You're almost done! \nPut this key into your account's bio: `"+CryptoJS.AES.encrypt("mt.co", message.author.id).toString()+"` \n\nThen type !register and the RSI Handle(s) \nIE: !register JamesDusky0 JamesDusky1";
