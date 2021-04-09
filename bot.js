731a2ef5b3962f1ce51b28dcf53e6cb26d84d828
Turan Ates <jamesdusky@dustytavern.com>
2021-04-09 11:28:33 -0400

    t

--- bot.js
+++ bot.js
@@ -355,7 +355,7 @@ async function registerUser(message, argz){
                         if(failedNames.length > 0){
                           var fString = failedNames.join(", ");
                         }
-                        console.log(message.author.username+"#"+message.author.discriminator+"\n\nRegistered: "+registeredNames.join(", ")+"\n\nFailed:"+failedNames.join(", ")+" (NO TOKEN)");
+                        console.log("\n"+message.author.username+"#"+message.author.discriminator+"\nRegistered: "+registeredNames.join(", ")+"\nFailed:"+failedNames.join(", ")+" (NO TOKEN)");
                       }
                       ii++;
                     }else{
