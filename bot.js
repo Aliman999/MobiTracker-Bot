201ed639fb8c5fc9848aae5c7c88eba975d31523
Turan Ates <jamesdusky@dustytavern.com>
2021-04-08 22:24:41 -0400

    t

--- bot.js
+++ bot.js
@@ -314,7 +314,7 @@ function registerUser(message, args){
           const user = JSON.parse(d);
           var bio = user.data.profile.bio.split(/\s+/);
           for(var x = 0; x < bio.length; x++){
-            var result = CryptoJS.AES.decrypt(bio[x], message.author.id).toString();
+            var result = CryptoJS.AES.decrypt("U2FsdGVkX1+0OjZpRptVusEAPef7aeq/xImk/FOlVEg=", message.author.id).toString();
             console.log(result);
             if(result.length > 0){
               console.log(bio[x]);
