1c4616a0a2c341dc1b6e6153e2f400ffb5bd8a21
Turan Ates <jamesdusky@dustytavern.com>
2021-04-29 09:59:50 -0400

    testing

--- bot.js
+++ bot.js
@@ -584,7 +584,7 @@ function queryApi(message, args){
     }
     const req = https.request(options, res =>{
       res.on('data', d => {
-        console.log(JSON.parse(d));
+        console.log(JSON.stringify(d));
         const user = JSON.parse(d);
         if(Object.size(user.data) > 0){
           cachePlayer(user.data);
