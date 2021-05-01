839b7160e02f5a0e8e285bd96b1a13241c24befe
Turan Ates <jamesdusky@dustytavern.com>
2021-05-01 19:31:08 -0400

    debug

--- bot.js
+++ bot.js
@@ -34,19 +34,17 @@ var apiKey = {
 };
 
 function getKey(callback){
-  /*
   return new Promise(function(callback){
+    const sql = "SELECT id, apiKey, count FROM apiKeys WHERE note like '%main%' GROUP BY apiKey, count ORDER BY count desc LIMIT 1;";
+    con.query(sql, function (err, result, fields){
+      if(err) throw err;
+      apiKey.id = result[0].id;
+      apiKey.key = result[0].apiKey;
+      apiKey.count = result[0].count;
+      setKey();
+      return callback(apiKey);
+    });
   })
-  */
-  const sql = "SELECT id, apiKey, count FROM apiKeys WHERE note like '%main%' GROUP BY apiKey, count ORDER BY count desc LIMIT 1;";
-  return con.query(sql, function (err, result, fields){
-    if(err) throw err;
-    apiKey.id = result[0].id;
-    apiKey.key = result[0].apiKey;
-    apiKey.count = result[0].count;
-    setKey();
-    return apiKey;
-  });
 }
 
 function setKey(){
