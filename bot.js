d3a014532e4c34db86593026143e0863498393c8
Turan Ates <jamesdusky@dustytavern.com>
2021-05-01 16:48:09 -0400

    Debug

--- bot.js
+++ bot.js
@@ -131,10 +131,7 @@ function numberWithCommas(x) {
 
 async function lookUp(message, args, finished = false, type){
   for(var i = 0; i < 1000; i++){
-    getKey()
-    .then(async function(result){
-      console.log(result);
-    })
+    console.log(getKey());
   }
   //message.channel.send(await queryApi(message, args, type, result))
   if(finished){
