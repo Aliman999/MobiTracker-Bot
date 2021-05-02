b3524149e5f98df03c227f4ecb0f46e3888670cb
Turan Ates <jamesdusky@dustytavern.com>
2021-05-02 01:28:29 -0400

    debug

--- bot.js
+++ bot.js
@@ -465,6 +465,7 @@ async function registerUser(message, argz, key){
           }
         }
       }else if(result.length > 0){
+        console.log(result);
         if(result[0].length > 0){
           addRSI(result);
         }else{
