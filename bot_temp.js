519a07e6cd76d9de1de474ca33fbc426a89d9b03
Turan Ates <jamesdusky@dustytavern.com>
2021-05-31 23:05:51 -0400

    debug

--- bot_temp.js
+++ bot_temp.js
@@ -17,9 +17,12 @@ var jwt = require('jsonwebtoken');
 var discordClients = [];
 var position = [];
 var update = [];
-const jobQueue = new Bottleneck({
-  maxConcurrent:1,
+const limiter = new Bottleneck({
+  maxConcurrent: 1,
   minTime: 333
+});
+const jobQueue = new Bottleneck({
+  maxConcurrent:1
 });
 const queueCounts = jobQueue.counts();
 
@@ -45,6 +48,20 @@ jobQueue.on("executing", function (info) {
   console.log(jobQueue.jobs("EXECUTING").join(", ")+" executing");
   console.log(jobQueue.counts());
   //info.args[3].edit("Running");
+});
+
+limiter.on("received", function (info) {
+  update[0].edit("Running");
+});
+
+limiter.on("executing", function (info) {
+  if(position[0].author.username != info.args[2].author.username){
+    position.shift();
+    update.shift();
+    for(){
+
+    }
+  }
 });
 
 const botToken = jwt.sign({ mtUser:{username:'mtcobot', cid: '0000001'} }, config.Secret, { algorithm: 'HS256' }, { 'iat':Math.floor(Date.now()/1000) });
@@ -99,10 +116,7 @@ function getKey(){
 async function addQueue(message, args){
   console.log(message.author.username+" started request for "+args.length+" searches.");
   var msg = await message.channel.send("Preparing your request");
-  jobQueue.schedule( { id:message.author.username }, ()=>{
-    const allTasks = tasksArray.map(() => lookUp(args.length, message, args, msg));
-    return Promise.all(allTasks);
-  })
+  jobQueue.schedule( { id:message.author.username }, lookUp, args.length, message, args, msg)
   .catch((error) => {
     if (error instanceof Bottleneck.BottleneckError) {
       msg.edit("You must wait for your current job to finish.");
