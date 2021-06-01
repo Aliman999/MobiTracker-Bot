6e08bf9827612b501f039b109f88f0e9a0a2eba2
Turan Ates <jamesdusky@dustytavern.com>
2021-05-31 20:33:50 -0400

    Debug

--- bot.js
+++ bot.js
@@ -87,7 +87,7 @@ function getKey(){
 async function addQueue(message, args){
   console.log(message.author.username+" started request for "+args.length+" searches.");
   var msg = await message.channel.send("Preparing your request");
-  queue.schedule( { id:"queue" }, lookUp, args.length, message, args, msg);
+  queue.schedule( { id:message.author.username }, lookUp, args.length, message, args, msg);
 }
 
 
