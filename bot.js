2745c19ee4dbf770aa60f663fe48e2db6d1f78b4
Turan Ates <jamesdusky@dustytavern.com>
2021-06-01 15:56:43 -0400

    Debug

--- bot.js
+++ bot.js
@@ -787,7 +787,7 @@ async function registerUser(message, argz){
     var ii = 0;
     var tries = 0;
     for(var i = 0; i < argz.length; i++){
-    console.log(i);
+    console.log(argz[i]);
       if(username.includes(argz[i])){
         alreadyLinked.push(argz[i]);
         username.splice(username.indexOf(argz[i]), 1);
