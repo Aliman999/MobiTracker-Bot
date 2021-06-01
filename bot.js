10e959546d93bd6896bf3f71da3e3dbe2b6ea383
Turan Ates <jamesdusky@dustytavern.com>
2021-06-01 14:48:56 -0400

    Debug

--- bot.js
+++ bot.js
@@ -40,7 +40,7 @@ limiter.on("executing", function (info) {
     update.shift();
     console.log(position[0].author.username+" job started");
     for(var ii = 0; ii < position.length; ii++){
-      console.log(position[ii].author.username+" is "+ii+" in queue");
+      console.log(position[ii].author.username+" is "+ii+" in queue | Priority: "+position[ii].author.prio);
       update[ii].edit(ii+" in Queue");
     }
   }
