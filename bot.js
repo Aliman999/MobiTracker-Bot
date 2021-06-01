274ef2789affa6d75141c7eaa1b8ce8e67dc5eac
Turan Ates <jamesdusky@dustytavern.com>
2021-06-01 19:57:12 -0400

    Debug

--- bot.js
+++ bot.js
@@ -691,65 +691,63 @@ async function registerUser(message, argz){
               res.on('end', function(){
                 const user = JSON.parse(body);
                 if(Object.keys(user.data).length > 0){
-                  //if(user.data.profile.id != "n/a"){
-                    if(user.data.profile.bio){
-                      const bio = user.data.profile.bio.split(/\s+/);
-                      for(var x = 0; x < bio.length; x++){
-                        var encrypted = bio[x];
-                        try{
-                          var result = CryptoJS.AES.decrypt(encrypted, message.author.id).toString(CryptoJS.enc.Utf8);
-                        }catch{
-                        }
-                        if(result == "mt.co"){
-                          if(!registeredNames.includes(user.data.profile.handle)){
-                            registeredNames.push(user.data.profile.handle);
-                            registeredCID.push(user.data.profile.id.substring(1));
-                          }
-                          x = bio.length
-                        }else{
-                          if(x == bio.length-1){
-                            failedNames.push(user.data.profile.handle);
-                          }
-                        }
-                      }
-                      if(ii == args.length-1){
-                        var rString = "", fString = "", drString = "", dfString = "";
-                        if(registeredNames.length > 0){
-                          rString = " | Registered: "+registeredNames.join(", ");
-                          drString = "Registered: "+registeredNames.join(", ")+" ";
-                        }
-                        if(failedNames.length > 0){
-                          fString = " | Failed: "+failedNames.join(", ")+" (No Token/Wrong Token)";
-                          dfString = dfString+"Failed: "+failedNames.join(", ")+" (No Token/Wrong Token)";
-                        }
-                        var finalString = rString+fString;
-                        console.log(message.author.username+"#"+message.author.discriminator+finalString);
-                        if(drString && !dfString){
-                          message.channel.send(drString);
-                        }else if (!drString && drString) {
-                          message.channel.send(dfString);
-                        }else{
-                          message.channel.send(drString+"\n"+dfString);
-                        }
-
-                        var password = CryptoJS.AES.encrypt("mt.co", message.author.id).toString();
-                        password = password.substring(password.length/2, password.length);
-
-                        const sql = "UPDATE discord SET cID = '"+JSON.stringify(registeredCID)+"', username = '"+JSON.stringify(registeredNames)+"', password = '"+password+"';";
-                        con.query(sql, function (err, result, fields) {
-                          if(err) throw err;
-                        });
-
-                      }
-                      ii++;
-                    }else{
-                      message.channel.send("Unfortunately we could not find "+user.data.profile.handle+"'s bio.");
-                      console.log(message.author.username+"#"+message.author.discriminator+" failed to register "+user.data.profile.handle+" (No Bio)");
+                  if(user.data.profile.bio){
+                    const bio = user.data.profile.bio.split(/\s+/);
+                    if(user.data.profile.id != "n/a"){
+                      user.data.profile.id.substring(1);
                     }
-                  //}else{
-                  //  message.channel.send(user.data.profile.handle+" unfortunately doesn't have a Citizen ID. \n\nThis is because:\n1. You don't own a Star Citizen Game Package\n2. Your game package was gifted and you need to add 45(USD) of credit to your account to get a Citizen ID.");
-                  //  console.log(message.author.username+"#"+message.author.discriminator+" failed to register "+user.data.profile.handle+" (No ID)");
-                  //}
+                    for(var x = 0; x < bio.length; x++){
+                      var encrypted = bio[x];
+                      try{
+                        var result = CryptoJS.AES.decrypt(encrypted, message.author.id).toString(CryptoJS.enc.Utf8);
+                      }catch{
+                      }
+                      if(result == "mt.co"){
+                        if(!registeredNames.includes(user.data.profile.handle)){
+                          registeredNames.push(user.data.profile.handle);
+                          registeredCID.push(user.data.profile.id);
+                        }
+                        x = bio.length
+                      }else{
+                        if(x == bio.length-1){
+                          failedNames.push(user.data.profile.handle);
+                        }
+                      }
+                    }
+                    if(ii == args.length-1){
+                      var rString = "", fString = "", drString = "", dfString = "";
+                      if(registeredNames.length > 0){
+                        rString = " | Registered: "+registeredNames.join(", ");
+                        drString = "Registered: "+registeredNames.join(", ")+" ";
+                      }
+                      if(failedNames.length > 0){
+                        fString = " | Failed: "+failedNames.join(", ")+" (No Token/Wrong Token)";
+                        dfString = dfString+"Failed: "+failedNames.join(", ")+" (No Token/Wrong Token)";
+                      }
+                      var finalString = rString+fString;
+                      console.log(message.author.username+"#"+message.author.discriminator+finalString);
+                      if(drString && !dfString){
+                        message.channel.send(drString);
+                      }else if (!drString && drString) {
+                        message.channel.send(dfString);
+                      }else{
+                        message.channel.send(drString+"\n"+dfString);
+                      }
+
+                      var password = CryptoJS.AES.encrypt("mt.co", message.author.id).toString();
+                      password = password.substring(password.length/2, password.length);
+
+                      const sql = "UPDATE discord SET cID = '"+JSON.stringify(registeredCID)+"', username = '"+JSON.stringify(registeredNames)+"', password = '"+password+"';";
+                      con.query(sql, function (err, result, fields) {
+                        if(err) throw err;
+                      });
+
+                    }
+                    ii++;
+                  }else{
+                    message.channel.send("Unfortunately we could not find "+user.data.profile.handle+"'s bio.");
+                    console.log(message.author.username+"#"+message.author.discriminator+" failed to register "+user.data.profile.handle+" (No Bio)");
+                  }
                 }else{
                   if(tries != 1){
                     console.log("Failed to find "+name+", retrying.");
@@ -823,65 +821,63 @@ async function registerUser(message, argz){
           res.on('end', function(){
             const user = JSON.parse(body);
             if(Object.keys(user.data).length > 0){
-              //if(user.data.profile.id != "n/a"){
-                if(user.data.profile.bio){
-                  const bio = user.data.profile.bio.split(/\s+/);
-                  for(var x = 0; x < bio.length; x++){
-                    var encrypted = bio[x];
-                    try{
-                      var crypto = CryptoJS.AES.decrypt(encrypted, message.author.id).toString(CryptoJS.enc.Utf8);
-                    }catch{
-                    }
-                    if(crypto == "mt.co"){
-                      if(username == null){
-                        username.push(user.data.profile.handle);
-                        registeredCID.push(user.data.profile.id.substring(1));
-                      }else{
-                        if(!username.includes(user.data.profile.handle)){
-                          username.push(user.data.profile.handle);
-                          registeredCID.push(user.data.profile.id.substring(1));
-                        }
-                      }
-                      x = bio.length;
-                    }else{
-                      if(x == bio.length-1){
-                        failedNames.push(user.data.profile.handle);
-                      }
-                    }
-                  }
-                  if(ii == argz.length-1){
-                    var rString = "", fString = "", aString = "";
-                    if(username.length > 0){
-                      rString = "Registered: "+username.join(", ")+" ";
-                    }
-                    if(alreadyLinked.length > 0){
-                      aString = "Already Linked: "+alreadyLinked.join(", ")+" ";
-                    }
-                    if(failedNames.length > 0){
-                      fString = "Failed: "+failedNames.join(", ")+" (No Token/Wrong Token)";
-                    }
-                    var finalString = rString+aString+fString;
-                    console.log(message.author.username+"#"+message.author.discriminator+" "+finalString);
-                    message.channel.send(finalString);
-
-                    var password = CryptoJS.AES.encrypt("mt.co", message.author.id).toString();
-                    password = password.substring(password.length/2, password.length);
-
-                    if(registeredCID.length > 0){
-                      const sql = "UPDATE discord SET cID = '"+JSON.stringify(registeredCID)+"', username = '"+JSON.stringify(username)+"' WHERE discID = "+message.author.id+";";
-                      con.query(sql);
-                    }
-
-                  }
-                  ii++;
-                }else{
-                  message.channel.send("Unfortunately we could not find "+user.data.profile.handle+"'s bio.");
-                  console.log(message.author.username+"#"+message.author.discriminator+" failed to register "+user.data.profile.handle+" (No Bio)");
+              if(user.data.profile.bio){
+                const bio = user.data.profile.bio.split(/\s+/);
+                if(user.data.profile.id != "n/a"){
+                  user.data.profile.id.substring(1);
                 }
-              //}else{
-              //  message.channel.send(user.data.profile.handle+" unfortunately doesn't have a Citizen ID. \n\nThis is because:\n1. You don't own a Star Citizen Game Package\n2. Your game package was gifted and you need to add 45(USD) of credit to your account to get a Citizen ID.");
-              //  console.log(message.author.username+"#"+message.author.discriminator+" failed to register "+user.data.profile.handle+" (No ID)");
-              //}
+                for(var x = 0; x < bio.length; x++){
+                  var encrypted = bio[x];
+                  try{
+                    var crypto = CryptoJS.AES.decrypt(encrypted, message.author.id).toString(CryptoJS.enc.Utf8);
+                  }catch{
+                  }
+                  if(crypto == "mt.co"){
+                    if(username == null){
+                      username.push(user.data.profile.handle);
+                      registeredCID.push(user.data.profile.id);
+                    }else{
+                      if(!username.includes(user.data.profile.handle)){
+                        username.push(user.data.profile.handle);
+                        registeredCID.push(user.data.profile.id);
+                      }
+                    }
+                    x = bio.length;
+                  }else{
+                    if(x == bio.length-1){
+                      failedNames.push(user.data.profile.handle);
+                    }
+                  }
+                }
+                if(ii == argz.length-1){
+                  var rString = "", fString = "", aString = "";
+                  if(username.length > 0){
+                    rString = "Registered: "+username.join(", ")+" ";
+                  }
+                  if(alreadyLinked.length > 0){
+                    aString = "Already Linked: "+alreadyLinked.join(", ")+" ";
+                  }
+                  if(failedNames.length > 0){
+                    fString = "Failed: "+failedNames.join(", ")+" (No Token/Wrong Token)";
+                  }
+                  var finalString = rString+aString+fString;
+                  console.log(message.author.username+"#"+message.author.discriminator+" "+finalString);
+                  message.channel.send(finalString);
+
+                  var password = CryptoJS.AES.encrypt("mt.co", message.author.id).toString();
+                  password = password.substring(password.length/2, password.length);
+
+                  if(registeredCID.length > 0){
+                    const sql = "UPDATE discord SET cID = '"+JSON.stringify(registeredCID)+"', username = '"+JSON.stringify(username)+"' WHERE discID = "+message.author.id+";";
+                    con.query(sql);
+                  }
+
+                }
+                ii++;
+              }else{
+                message.channel.send("Unfortunately we could not find "+user.data.profile.handle+"'s bio.");
+                console.log(message.author.username+"#"+message.author.discriminator+" failed to register "+user.data.profile.handle+" (No Bio)");
+              }
             }else{
               if(tries != 1){
                 console.log("Failed to find "+name+", retrying.");
