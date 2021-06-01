952b1695225bdceb713192e4131481cec65dfc7c
Turan Ates <jamesdusky@dustytavern.com>
2021-05-31 22:48:47 -0400

    debug

--- bot.js
+++ bot.js
@@ -50,9 +50,7 @@ jobQueue.on("executing", function (info) {
 });
 
 limiter.on("executing", function (info) {
-  if(position.indexOf(info.args[2]) < 0){
-
-  }
+  console.log(position[0].message.author.username+" | "info.args[2].message.author.username);
 });
 
 const botToken = jwt.sign({ mtUser:{username:'mtcobot', cid: '0000001'} }, config.Secret, { algorithm: 'HS256' }, { 'iat':Math.floor(Date.now()/1000) });
