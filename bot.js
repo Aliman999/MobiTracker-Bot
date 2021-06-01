1ac26240ccc68028713e8f259a1e0f7145f81ee5
Turan Ates <jamesdusky@dustytavern.com>
2021-06-01 11:53:11 -0400

    debug

--- bot.js
+++ bot.js
@@ -223,7 +223,7 @@ async function lookUp(count, message, args, msg){
   }
   const query = function(args, keys, message, i){
     for(var i = 0; i < args.length; i++){
-      final.schedule(async () => {
+      final.schedule(() => {
         args[i] = args[i].replace(/[^\-a-zA-Z0-9]/g, '_');
         if(message.author.id != "751252617451143219"){
           if(message.channel.type == "text"){
@@ -232,7 +232,7 @@ async function lookUp(count, message, args, msg){
             console.log(message.author.username+'#'+message.author.discriminator+' searched for '+args[i]+' in '+message.channel.type+'s');
           }
         }
-        message.channel.send(await queryApi(args[i], keys[i]));
+        message.channel.send(queryApi(args[i], keys[i]));
       });
     }
   }
