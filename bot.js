0d4ac562935281a940437008a15acc1220c57b59
Turan Ates <jamesdusky@dustytavern.com>
2021-06-02 19:07:02 -0400

    debug

--- bot.js
+++ bot.js
@@ -573,7 +573,7 @@ client.on('message', async message => {
     if(args.length > 1){
       addQueue(message, args);
     }else{
-      var msg = await message.channel.send("Preparing your request");
+      var msg = await message.channel.send("**[STATUS]: ** \u231B	 ```Our microtech datacenters are processing your request.```");
       lookUp(args.length, message, args, msg);
     }
   }
