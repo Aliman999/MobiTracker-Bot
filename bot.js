657e273b8eb287de9796ab8b74a08df789fbaa8f
Turan Ates <jamesdusky@dustytavern.com>
2021-06-01 20:41:49 -0400

    debug

--- bot.js
+++ bot.js
@@ -238,9 +238,9 @@ async function lookUp(count, message, args, msg){
   var percent = 0;
   message.author.prio = await getPrio(message.author.id);
   for(var i = 0; i < args.length; i++){
-    await getKey(args.length).then((key) => {
+    await getKey(args.length).then(async (key) => {
       keys.push(key);
-      await msg.edit(loading(percent, args.length));
+      msg.edit(loading(percent, args.length));
       percent++;
     });
   }
