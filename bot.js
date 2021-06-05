cdb9d229c637ad45b6a104b29f809b416103a16d
Turan Ates <jamesdusky@dustytavern.com>
2021-06-05 15:57:29 -0400

    Debug

--- bot.js
+++ bot.js
@@ -67,7 +67,7 @@ group.on("created", (limiter, key) => {
   })
   limiter.once("executing", function(info){
     for(var ind = 0; ind < position.length; ind++){
-      console.log(position[ind].id+" | "+info.id);
+      console.log(position[ind].id+" | "+info[0].id);
       if(position[ind].id === info.id){
         position[ind].msg.edit("**[STATUS]: ** \u2699 ```Running.```");
         console.log(position[ind].id+' running');
