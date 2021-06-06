0bc7a309e709b3d5b2403b72a7452501c148d8b8
Turan Ates <jamesdusky@dustytavern.com>
2021-06-06 14:48:24 -0400

    Debug

--- bot.js
+++ bot.js
@@ -67,7 +67,8 @@ group.on("created", (limiter, key) => {
   limiter.on("done", function(info){
     console.log(info);
     console.log(count);
-    if(++count == info.args[4]){
+    count++;
+    if(count == info.args[4]){
       info.args[2].channel.send("**[STATUS]: ** \uF4BE ```Finished "+info.args[5]+" searches.```");
       console.log(info.args[4]+' Finished');
     }
