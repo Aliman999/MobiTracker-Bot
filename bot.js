565915554466b645d43a825944410d8fd28f58b5
Turan Ates <jamesdusky@dustytavern.com>
2021-06-06 14:46:10 -0400

    Debug

--- bot.js
+++ bot.js
@@ -65,13 +65,13 @@ group.on("created", (limiter, key) => {
   limiter.once("received", function(info){
   })
   limiter.on("done", function(info){
+    count++;
     console.log(info);
     console.log(count);
     if(count == info.args[4]){
       info.args[2].channel.send("**[STATUS]: ** \uF4BE ```Finished "+info.args[5]+" searches.```");
       console.log(position[ind].id+' Finished');
     }
-    count++;
   })
 
   limiter.on("failed", async (error, jobInfo) => {
