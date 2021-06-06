2a4435acd5d8564a85abe0fc27dcaf16dd283b5c
Turan Ates <jamesdusky@dustytavern.com>
2021-06-06 16:43:44 -0400

    Debug

--- bot.js
+++ bot.js
@@ -287,8 +287,9 @@ async function lookUp(count, message, args, msg){
   }
   for(var i = 0; i < args.length; i++){
     var i = i;
-    group.key(message.author.username).schedule((i) =>{
-      query(args[i], keys[i], message, msg, message.author.username, args.length)
+    group.key(message.author.username).schedule(async (i) =>{
+      console.log(i);
+      await query(args[i], keys[i], message, msg, message.author.username, args.length)
       .then((result)=>{
         if(result){
           throw new Error(result.data);
