298029c46610c3b68f1a13e5d390e8d1ef6176a9
Turan Ates <jamesdusky@dustytavern.com>
2021-06-09 13:14:30 -0400

    Debug

--- bot.js
+++ bot.js
@@ -235,15 +235,7 @@ console.log = function(msg) {
 
 var logSave = console.save;
 console.save = function(msg) {
-  const date = new Date();
-  const day = ("0" + date.getDate()).slice(-2);
-  const month = ("0" + (date.getMonth() + 1)).slice(-2);
-  const year = date.getFullYear();
-  fs.appendFile('/home/ubuntu/logs/bot.log', "["+month+"/"+day+"/"+year+" "+date.toLocaleTimeString('en-US')+"]"+" - "+msg+'\n', function(err) { if(err) {
-      return trueLog(err);
-    }
-  });
-  logSave(msg);
+  trueLog(msg);
 }
 
 console.save("test");
