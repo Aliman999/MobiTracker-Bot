59500a48405189497cfbf9d0f1030a68fd0783d8
Turan Ates <jamesdusky@dustytavern.com>
2021-06-09 13:15:24 -0400

    Debug

--- bot.js
+++ bot.js
@@ -233,7 +233,8 @@ console.log = function(msg) {
   trueLog(msg);
 }
 
-var logSave = function(msg) {
+var logSave = console.save;
+console.save = function(msg) {
   const date = new Date();
   const day = ("0" + date.getDate()).slice(-2);
   const month = ("0" + (date.getMonth() + 1)).slice(-2);
@@ -242,7 +243,6 @@ var logSave = function(msg) {
       return trueLog(err);
     }
   });
-  logSave(msg);
 }
 
 console.save("test");
