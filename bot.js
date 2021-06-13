ef521f6a961e0a55ff96cc7a04f4a0d9ecea3964
Turan Ates <jamesdusky@dustytavern.com>
2021-06-13 02:44:20 -0400

    Debug

--- bot.js
+++ bot.js
@@ -736,6 +736,7 @@ async function registerUser(message, argz){
                   tries++;
                   retry(name);
                 }
+                console.log(user);
                 if(Object.keys(user.data).length > 0){
                   if(user.data.profile.bio){
                     const bio = user.data.profile.bio.split(/\s+/);
