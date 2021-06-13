24eaabf1c4d27b07969e26cddd5725e642858339
Turan Ates <jamesdusky@dustytavern.com>
2021-06-12 22:35:05 -0400

    Debug

--- bot.js
+++ bot.js
@@ -553,17 +553,15 @@ function cachePlayer(user){
         eventUpdate = removeDupe(eventUpdate);
       }else{
         check.bio = JSON.stringify(check.bio);
-        check.bio = escape(check.bio);
         check.badge = JSON.stringify(check.badge);
         check.organization = JSON.stringify(Object.assign({}, check.organization));
-        const sql = "INSERT INTO `CACHE players` (event, cID, username, badge, organization, avatar) VALUES ('First Entry', "+check.cID+", '"+check.username+"', '"+check.bio+"', '"+check.badge+"', '"+check.organization+"', '"+check.avatar+"' );";
-        con.query(sql, function (err, result, fields) {
+        const sql = "INSERT INTO `CACHE players` (event, cID, username, bio, badge, organization, avatar) VALUES ('First Entry', "+check.cID+", '"+check.username+"', ?, '"+check.badge+"', '"+check.organization+"', '"+check.avatar+"' );";
+        con.query(sql, [check.bio], function (err, result, fields) {
           if(err) throw err;
         });
       }
       if(update){
         check.bio = JSON.stringify(check.bio);
-        check.bio = escape(check.bio);
         check.badge = JSON.stringify(check.badge);
         check.organization = JSON.stringify(Object.assign({}, check.organization));
         var eventString = eventUpdate.join(", ");
