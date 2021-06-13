4743f59f64180d08b66bb5ee533f9a5da3d14205
Turan Ates <jamesdusky@dustytavern.com>
2021-06-12 22:25:02 -0400

    Debug

--- bot.js
+++ bot.js
@@ -556,7 +556,7 @@ function cachePlayer(user){
         check.bio = con.escape(check.bio);
         check.badge = JSON.stringify(check.badge);
         check.organization = JSON.stringify(Object.assign({}, check.organization));
-        const sql = "INSERT INTO `CACHE players` (event, cID, username, badge, organization, avatar) VALUES ('First Entry', "+check.cID+", '"+check.username+"', '"+check.bio+"', '"+check.badge+"', '"+check.organization+"', '"+check.avatar+"' );";
+        const sql = "INSERT INTO `CACHE players` (event, cID, username, badge, organization, avatar) VALUES ('First Entry', "+check.cID+", "+check.username+", "+check.bio+", "+check.badge+", "+check.organization+", "+check.avatar+" );";
         con.query(sql, function (err, result, fields) {
           if(err) throw err;
         });
@@ -567,7 +567,7 @@ function cachePlayer(user){
         check.badge = JSON.stringify(check.badge);
         check.organization = JSON.stringify(Object.assign({}, check.organization));
         var eventString = eventUpdate.join(", ");
-        const sql = "INSERT INTO `CACHE players` (event, cID, username, bio, badge, organization, avatar) VALUES ('"+eventString+"', "+check.cID+", '"+check.username+"', `"+check.bio+"`, '"+check.badge+"', '"+check.organization+"', '"+check.avatar+"');";
+        const sql = "INSERT INTO `CACHE players` (event, cID, username, bio, badge, organization, avatar) VALUES ("+eventString+", "+check.cID+", "+check.username+", "+check.bio+", "+check.badge+", "+check.organization+", "+check.avatar+");";
         con.query(sql, function (err, result, fields) {
           if(err) throw err;
         });
