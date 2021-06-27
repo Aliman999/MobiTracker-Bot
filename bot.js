c9324e25e69a5e64dcb29f786b4a5dd34b12c8f2
Turan Ates <jamesdusky@dustytavern.com>
2021-06-27 13:01:57 -0400

    Debug

--- bot.js
+++ bot.js
@@ -119,8 +119,7 @@ client.on("ready", () => {
   const list = ["for !help", "for new Contracts", "for new Applicants", "for new Reviews"];
   function loopStatus(){
     setTimeout(function(){
-      const guild = client.guilds.fetch("826925430773645332");
-      console.log(guild.members.cache.size);
+      console.log(client.channels.cache.get("829705342618697799"));
       /*
       var memberCount = guild.members.filter(member => !member.user.bot).size;
       console.log(memberCount);
