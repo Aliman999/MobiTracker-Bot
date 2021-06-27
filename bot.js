dae796fffd9cd99ee7a05e580d4042aa26c98ba2
Turan Ates <jamesdusky@dustytavern.com>
2021-06-27 13:16:16 -0400

    Debug

--- bot.js
+++ bot.js
@@ -120,6 +120,7 @@ client.on("ready", () => {
   function loopStatus(){
     setTimeout(function(){
       var guild = client.guilds.cache.get("826925430773645332");
+      console.log(guild);
       guild.members.forEach((item, i) => {
         console.log(item);
       });
