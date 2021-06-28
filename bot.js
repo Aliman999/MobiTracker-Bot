525845c38cacd4cb337953925a8e1ea1179d2826
Turan Ates <jamesdusky@dustytavern.com>
2021-06-28 18:56:04 -0400

    Debug

--- bot.js
+++ bot.js
@@ -83,6 +83,7 @@ group.on("created", (limiter, key) => {
     console.log(count+" | "+info.args[5]+" - "+info.args[6]);
     if(count == info.args[5]){
       for(var iii = 0; iii < groups.length; iii++){
+        console.log(groups[iii]);
         if(groups[iii] === info.options.id){
           console.log(groups[iii]+ " | "+info.options.id);
           groups.splice(iii, 1);
